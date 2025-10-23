90048223bba768529411270c3f6a11be3f3cb190
P-K-03 <PranjalKumar03@protonmail.com>
2025-10-23 11:45:10 +0000

    Added the LeafNode child class

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -11,15 +11,31 @@ class HTMLNode:
         raise NotImplementedError("to_html method not implemented")
     
     def props_to_html(self):
-        prop_html: str = " "  #initialized with space
+        prop_html: str = "" 
         if self.props is None:
             return prop_html
         
         for k,v in self.props.items():
-            prop_html  += f"{k}={v} "   # space at the end
+            prop_html  += f" {k}={v}"   # space at the start of every property
         
         return prop_html
     
     def __repr__(self):
-        return f"HTMLNode(tag: {self.tag},\nvalue: {self.value},\nchildren: {self.children},\nprops: {self.props})"
+        return f"HTMLNode(tag: {self.tag}, value: {self.value}, children: {self.children}, props: {self.props})"
     
+class LeafNode(HTMLNode):
+    def __init__(self, tag: str, value: str, props: Dict = None):
+        super().__init__(tag, value, None, props)
+    
+    def to_html(self):
+        if self.value == None:
+            raise ValueError("All leaf nodes must have a value")
+        elif self.tag == None:
+            # return the value as raw text
+            return self.value
+        else:
+            # return the HTML string
+            return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>" 
+    
+    def __repr__(self):
+        return f"LeafNode(tag: {self.tag}, value: {self.value}, props: {self.props})"
