231d76d2e2391152705df3c2bd7b6143891f71f2
P-K-03 <PranjalKumar03@protonmail.com>
2025-10-31 08:46:36 +0000

    Added functions to extract images and links from markdown

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,3 +1,5 @@
+import re
+
 from typing import List, Dict
 from textnode import TextNode, TextType
 
@@ -22,7 +24,7 @@ def split_nodes_delimiter(old_nodes: List[TextNode], delimiter: str, text_type: 
             for i in range(0,len(sub_nodes)):
 
                 # If the element is empty, don't create a textnode 
-                if sub_nodes[i] != "":
+                if sub_nodes[i] == "":
                     continue
 
                 # Elements at the odd indexes will be between the delimiters
@@ -40,11 +42,28 @@ def split_nodes_delimiter(old_nodes: List[TextNode], delimiter: str, text_type: 
 
     return resultant_nodes   
 
+def extract_markdown_images(text: str):
+    """Takes raw markdown text and returns a list of tuples. Each tuple contains the alt text and the URL of any markdown images"""
+    markdown_image_regex: str = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches: List = re.findall(markdown_image_regex, text)
+    return matches
 
-# node = TextNode("**How to Train Your Dragon** is a 2025 American fantasy adventure film", TextType.TEXT)
-# new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
-# print(new_nodes)
+def extract_markdown_links(text: str):
+    """Extracts markdown links from the text. Returns tuples of anchor text and URLs"""
+    markdown_link_regex: str = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches: List = re.findall(markdown_link_regex, text)
+    return matches
 
-# node = TextNode("Nico Parker played Sarah Miller in the first season of the HBO series _The Last of Us_", TextType.TEXT)
-# new_nodes = split_nodes_delimiter([node], "_", TextType.ITALIC)
-# print(new_nodes)
+# print(extract_markdown_images("This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"))
+# print(extract_markdown_links("This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"))
+
+
+# This doesn't work
+# markdown_string: str = """Ben was an ordinary 10-year-old boy until he found the Omnitrix, a powerful watch-like device that allowed him to turn into 10 different aliens.
+# ![Ben Tennyson](https://ben10.fandom.com/wiki/Ben_Tennyson_(Classic)).
+# Ben's most powerful alien is Alien X, a Celestial Sapien. This article redirects to [Alien X](https://ben10.fandom.com/wiki/Alien_X_(Classic))."""
+
+# output_image = extract_markdown_images(markdown_string)
+# output_link = extract_markdown_links(markdown_string)
+# print(output_image)
+# print(output_link)
